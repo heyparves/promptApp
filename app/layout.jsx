271bf8852373a7e4c2aed85d5829336435e4bb59
heyparves <heyparveshere@gmail.com>
2023-08-15 17:56:51 +0600

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,19 +4,19 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  title: "YourPrompts",
+  description: "Explore & Share Your Prompts",
 };
 
 const RootLayout = ({ children }) => (
-  <html lang='en'>
+  <html lang="en">
     <body>
       <Provider>
-        <div className='main'>
-          <div className='gradient' />
+        <div className="main">
+          <div className="gradient" />
         </div>
 
-        <main className='app'>
+        <main className="app">
           <Nav />
           {children}
         </main>
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
-import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { data: session } = useSession();
@@ -34,76 +34,78 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   }, []);
 
   return (
-    <div className={`prompt_card ${loading ? "loading" : ""}`}>
-      <div className="flex justify-between items-start gap-5">
-        <div
-          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
-          onClick={handleProfileClick}
+    <SkeletonTheme baseColor="#202020" highlightColor="#444">
+      <div className={`prompt_card ${loading ? "loading" : ""}`}>
+        <div className="flex justify-between items-start gap-5">
+          <div
+            className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+            onClick={handleProfileClick}
+          >
+            {loading ? (
+              <Skeleton circle={true} width={40} height={40} />
+            ) : (
+              <>
+                <Image
+                  src={post.creator?.image}
+                  alt="user_image"
+                  width={40}
+                  height={40}
+                  className="rounded-full object-contain"
+                />
+                <div className="flex flex-col">
+                  <h3 className="font-satoshi font-semibold text-white">
+                    {post.creator?.username}
+                  </h3>
+                  <p className="font-inter text-sm text-white">
+                    {post.creator?.email}
+                  </p>
+                </div>
+              </>
+            )}
+          </div>
+
+          <div className="copy_btn" onClick={handleCopy}>
+            <Image
+              src={
+                copied === post.prompt
+                  ? "/assets/icons/tick.svg"
+                  : "/assets/icons/copy.svg"
+              }
+              alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+              width={12}
+              height={12}
+            />
+          </div>
+        </div>
+
+        <p className="my-4 font-satoshi text-sm text-white">
+          {loading ? <Skeleton count={3} /> : post.prompt}
+        </p>
+        <p
+          className="font-inter text-sm text-white cursor-pointer"
+          onClick={() => handleTagClick && handleTagClick(post.tag)}
         >
-          {loading ? (
-            <Skeleton circle={true} width={40} height={40} />
-          ) : (
-            <>
-              <Image
-                src={post.creator?.image}
-                alt="user_image"
-                width={40}
-                height={40}
-                className="rounded-full object-contain"
-              />
-              <div className="flex flex-col">
-                <h3 className="font-satoshi font-semibold text-white">
-                  {post.creator?.username}
-                </h3>
-                <p className="font-inter text-sm text-white">
-                  {post.creator?.email}
-                </p>
-              </div>
-            </>
-          )}
-        </div>
+          {loading ? <Skeleton width={50} /> : `#${post.tag}`}
+        </p>
 
-        <div className="copy_btn" onClick={handleCopy}>
-          <Image
-            src={
-              copied === post.prompt
-                ? "/assets/icons/tick.svg"
-                : "/assets/icons/copy.svg"
-            }
-            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
-            width={12}
-            height={12}
-          />
-        </div>
+        {session?.user?.id === post.creator?._id && pathName === "/profile" && (
+          <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+            <p
+              className="font-inter text-sm green_gradient cursor-pointer"
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p
+              className="font-inter text-sm orange_gradient cursor-pointer"
+              onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )}
       </div>
-
-      <p className="my-4 font-satoshi text-sm text-white">
-        {loading ? <Skeleton count={3} /> : post.prompt}
-      </p>
-      <p
-        className="font-inter text-sm text-white cursor-pointer"
-        onClick={() => handleTagClick && handleTagClick(post.tag)}
-      >
-        {loading ? <Skeleton width={50} /> : `#${post.tag}`}
-      </p>
-
-      {session?.user?.id === post.creator?._id && pathName === "/profile" && (
-        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-          <p
-            className="font-inter text-sm green_gradient cursor-pointer"
-            onClick={handleEdit}
-          >
-            Edit
-          </p>
-          <p
-            className="font-inter text-sm orange_gradient cursor-pointer"
-            onClick={handleDelete}
-          >
-            Delete
-          </p>
-        </div>
-      )}
-    </div>
+    </SkeletonTheme>
   );
 };
 
