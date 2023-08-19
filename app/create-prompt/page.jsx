81333953056374f24f27da6e231f9496d7fc6306
Parves <95443838+heyparves@users.noreply.github.com>
2023-08-19 17:28:51 +0600

    Update page.jsx

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,50 +1,59 @@
 "use client";
-
-import { useState } from "react";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  const [submitting, setSubmitting] = useState(false);
   const router = useRouter();
-  const { data: session } = useSession();
-
-  const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
+  const { data: session } = useSession(); // Correct placement
 
   const createPrompt = async (e) => {
+    // prevent default browser behavior
     e.preventDefault();
-    setIsSubmitting(true);
+    setSubmitting(true);
 
     try {
       const response = await fetch("/api/prompt/new", {
         method: "POST",
         body: JSON.stringify({
-          prompt: post.prompt,
           userId: session?.user.id,
+          prompt: post.prompt,
           tag: post.tag,
         }),
       });
 
       if (response.ok) {
-        router.push("/");
+        router.push("/"); // Redirect after successful submission
       }
     } catch (error) {
       console.log(error);
     } finally {
-      setIsSubmitting(false);
+      setSubmitting(false);
     }
   };
 
   return (
-    <Form
-      type='Create'
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={createPrompt}
-    />
+    <div>
+      {session ? (
+        <Form
+          type="Create"
+          post={post}
+          setPost={setPost}
+          submitting={submitting}
+          handleSubmit={createPrompt}
+        />
+      ) : (
+        <div className="w-full max-w-full bg-red-100 px-10 py-4 border-2 border-red-500">
+          Please log in to create a prompt.
+        </div>
+      )}
+    </div>
   );
 };
 
